3970d71c9b11599c87d5f8a3b4f2423d59ac90f7
sharpshot124 <sharpshot124@gmail.com>
2020-09-05 20:11:45 -0700

    Created basic template objects and scene

---
+++ Assets/Scripts/Systems/Level/LevelBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] biomes;
+
+    [SerializeField]
+    GameObject[] enemies;
+
+    [SerializeField]
+    Object[] objectives;
+
+    public void Build()
+    {
+
+    }
+}
