316136088e22ce3af88cb5338803de937314996a
sharpshot124 <sharpshot124@gmail.com>
2020-09-12 19:35:57 -0700

    Created settings/pause menu and Settings.cs

---
+++ Assets/Scripts/UI/ResolutionDropdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionDropdown : Dropdown
+{
+    new List<OptionData> options
+    {
+        get
+        {
+            base.options.Clear();
+            foreach (var setting in Screen.resolutions)
+            {
+                base.options.Add(new OptionData(string.Format("{0}x{1}", Screen.width, Screen.height)));
+            }
+            return base.options;
+        }
+
+        set
+        {
+            base.options = value;
+        }
+    }
+}
