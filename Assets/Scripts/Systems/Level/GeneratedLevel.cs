ae9c2b73289f327f730dc3d07e2764889053c00c
sharpshot124 <sharpshot124@gmail.com>
2020-09-26 13:04:59 -0700

    Added bolt, redesigned AI state machine, fixed player tile bugs

--- Assets/Scripts/Systems/Level/GeneratedLevel.cs
+++ Assets/Scripts/Systems/Level/GeneratedLevel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-[CreateAssetMenu(fileName ="New_Level", menuName = "Custom/Levels/Basic Level")]
+[CreateAssetMenu(fileName ="New_Level", menuName = "Custom/Levels/New Level")]
 public class GeneratedLevel : ScriptableObject, ILevel
 {
     public float size;
--- Assets/Scripts/Systems/Level/RandomZone.cs
+++ Assets/Scripts/Systems/Level/RandomZone.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New_RandomZone", menuName = "Custom/Levels/Zones/Random")]
+[CreateAssetMenu(fileName = "New_RandomZone", menuName = "Custom/Levels/New Zone")]
 public class RandomZone : ScriptableObject, IZone
 {
     [SerializeField] List<Object> _plots;
